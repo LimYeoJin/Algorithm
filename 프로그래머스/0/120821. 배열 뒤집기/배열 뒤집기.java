e80860e521375ee91f7c3fe3a8523d319af90f39
LimYeoJin <wlsxor5828@naver.com>
2024-07-23 23:55:49 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.03 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[num_list.length];
+        int initIndex = 0;
+        for(int i=num_list.length-1;i>=0;i--) {
+            answer[initIndex++] = num_list[i];
+        }
+        return answer;
+    }
+}
